66fdde5899ed3252fb766c33f2bcd9f1c1adde8e
Mathias <mathiasminacapilli@gmail.com>
2019-09-01 19:36:28 -0300

    Added the use of react hooks in the component person

--- src/App.js
+++ src/App.js
@@ -3,13 +3,36 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  //state can be only managed inside a class that extends Component
+  //If state changes React will re-render the page in order to show the changes
+  state = {
+    persons: [
+      { name: 'Mathias', age: 20 },
+      { name: 'Pepe', age: 999 }
+    ]
+  }
+
+  //The last word, Handler, is in order to say that this method is not going
+  //to be called but it will be executed when some event happens
+  switchNameHandler = () => {
+    //console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Mathias Minacapilli'
+    this.setState({
+      persons: [
+        { name: 'Mathias Minacapilli', age: 20 },
+        { name: 'Pepe', age: 1000 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person name="Mathias" age="20" />
-        <Person name="Pepe" age="999">My hobbies: Racing</Person>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 /* props is an object that has the properties of the component */
 /* props.children refers to the plain text between the opening and closing tag of our
 component */
-const person = (props) => {
+const Person = (props) => {
+
+    //personsState gives access to the actual state object
+    //setPersonsState is a function that allows us to update the actual state
+    const [ personsState, setPersonsState ] = useState({
+        race: 'Human'
+    });
+
+    const changeRaceHandler = () => {
+        setPersonsState({
+            race: 'orc'
+        })
+    };
+
     return (
         <div>
             <p>I'm {props.name} and I am {props.age} years old!</p>
+            <p>My race is: {personsState.race}</p>
+            <button onClick={changeRaceHandler}>Change race</button>
             <p>{props.children}</p>
         </div>
     );
 };
 
-export default person;
+export default Person;
