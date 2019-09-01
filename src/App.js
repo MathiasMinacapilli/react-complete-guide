7705129b0e073f0cd433a65411ed3ddc48ef2825
Mathias <mathiasminacapilli@gmail.com>
2019-09-01 18:23:32 -0300

    added pops parameter to the component person in order to do it dinamic

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person />
+        <Person name="Mathias" age="20" />
+        <Person name="Pepe" age="999">My hobbies: Racing</Person>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const person = () => {
-    return <p>I'm a Person!</p>
+/* props is an object that has the properties of the component */
+/* props.children refers to the plain text between the opening and closing tag of our
+component */
+const person = (props) => {
+    return (
+        <div>
+            <p>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+        </div>
+    );
 };
 
 export default person;
