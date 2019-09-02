7acc312113c3e55c6213d946788068343f8c9b63
gcolacurcio21 <gcolacurcio21@gmail.com>
2019-09-02 15:30:32 -0300

    Changed Persons and Person component to class components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,34 +1,38 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 
 import classes from './Person.css';
 
 /* props is an object that has the properties of the component */
 /* props.children refers to the plain text between the opening and closing tag of our
 component */
-const Person = (props) => {
-
-    //personsState gives access to the actual state object
-    //setPersonsState is a function that allows us to update the actual state
-    const [ personsState, setPersonsState ] = useState({
-        race: 'Human'
-    });
-
-    const changeRaceHandler = () => {
-        setPersonsState({
-            race: 'orc'
+class Person extends Component {
+    render() {
+        //personsState gives access to the actual state object
+        //setPersonsState is a function that allows us to update the actual state
+        /* 
+        const [ personsState, setPersonsState ] = useState({
+            race: 'Human'
         });
-    };
 
-    return (
-        <div className={classes.Person}>
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-            <input type="text" value={props.name} onChange={props.changed} />
-            <p>My race is: {personsState.race}</p>
-            <button onClick={changeRaceHandler}>Change race</button>
-            <p>{props.children}</p>
-            <button onClick={props.clickDelete}>Delete</button>
-        </div>
-    );
+        const changeRaceHandler = () => {
+            setPersonsState({
+                race: 'orc'
+            });
+        }; 
+        <p>My race is: {personsState.race}</p>
+        <button onClick={changeRaceHandler}>Change race</button>
+        */
+
+        return (
+            <div className={classes.Person}>
+                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+                <input type="text" value={this.props.name} onChange={this.props.changed} />
+                
+                <p>{this.props.children}</p>
+                <button onClick={this.props.clickDelete}>Delete</button>
+            </div>
+        );
+    }
 };
 
 //If I put Radium(Person) I get an error
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // Components
 import Person from './Person/Person';
@@ -6,18 +6,20 @@ import Person from './Person/Person';
 // Errors
 import ErrorBoundary from '../../ErrorBoundary/ErrorBoundary'
 
-const persons = props => {
-    return (
-        props.persons.map((person, index) => {
-            return <ErrorBoundary key={person.id}>
-              <Person 
-              clickDelete={() => props.deleteClicked(index)}
-              name={person.name} 
-              age={person.age}
-              changed={(event) => props.nameChanged(event, person.id)} />
-            </ErrorBoundary>
-        })
-    );
+class Persons extends Component {
+    render() {
+        return (
+            this.props.persons.map((person, index) => {
+                return <ErrorBoundary key={person.id}>
+                <Person 
+                clickDelete={() => this.props.deleteClicked(index)}
+                name={person.name} 
+                age={person.age}
+                changed={(event) => this.props.nameChanged(event, person.id)} />
+                </ErrorBoundary>
+            })
+        );
+    }
 }
 
-export default persons;
+export default Persons;
